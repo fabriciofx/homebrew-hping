7704197c6b9c44f53e843c3a618a97b33fb6c1c8
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-08-31 18:15:43 -0300

    Fix dependencies order

--- Formula/h/hping3.rb
+++ Formula/h/hping3.rb
@@ -5,9 +5,8 @@ class Hping3 < Formula
   sha256 "6a530f2d3ccf26766cb662c9e5ffc651e1e707a667b34962b42a41157ec619c4"
   license "GPL-2.0-or-later"
 
-  # Sort dependencies in alphabetical order
-  depends_on "libpcap"
   depends_on "pkgconf" => :build
+  depends_on "libpcap"
   depends_on "tcl-tk"
 
   def install
